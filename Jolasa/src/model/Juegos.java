644ab92e9725719c598d19a80fef87363e4f3b2f
asier <asier@192.168.0.26>
2021-02-16 16:03:59 +0100

    jolasa ping pong

--- Jolasa/src/model/Juegos.java
+++ Jolasa/src/model/Juegos.java
@@ -6,13 +6,17 @@
 package model;
 
 import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -20,6 +24,7 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import letratipoa.Fuentes;
 
@@ -38,25 +43,38 @@ public class Juegos<bolita> extends javax.swing.JFrame implements KeyListener {
     /**
      * Launch the application.
      *
+     * @param args
      * @throws InterruptedException //OJO AQUI
      */
     public static void main(String[] args) throws InterruptedException {
         Juegos frame = new Juegos();
         frame.setVisible(true);
 
-        for (int i = 0; i < 179; i = i + 1) {
+        for (int i = 0; i < 179; i++) {
+            Thread.sleep(180 - i);
+            frame.createBufferStrategy(3);
             frame.repaint();
             frame.moverMundo();
+            BufferStrategy strategy = frame.getBufferStrategy();
+            Graphics g = strategy.getDrawGraphics();
+            g.dispose();
+            strategy.show();
 
-            Thread.sleep(180 - i);
         }
         Thread.sleep(2);
+
     }
 
     /**
      * Constructor de la clase juego.
      */
     public Juegos() {
+        init();
+
+    }
+
+    public final void init() {
+
         setTitle("Ping Pong ");
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -88,29 +106,28 @@ public class Juegos<bolita> extends javax.swing.JFrame implements KeyListener {
 
     }
 
-    @Override
     public void paint(Graphics g) {
         super.paint(g);
+
         Graphics2D g2d = (Graphics2D) g;
         getContentPane().setBackground(Color.black);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, (RenderingHints.VALUE_ANTIALIAS_ON));
-        g.setColor(Color.WHITE);
-        g.fillRect(310, 20, 15, 450);
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(310, 20, 15, 450);
         g2d.setColor(Color.RED);
         raquetita1.pintarRaqueta1(g2d);
         g2d.setColor(Color.YELLOW);
         raquetita2.pintarRaqueta2(g2d);
         g2d.setColor(Color.WHITE);
         bolita.pintarBola(g2d);
+
     }
-    
 
     public void moverMundo() {
         bolita.moverBola();
         if (colision()) {
             bolita.rebotaBola();
             golpes = golpes + 1;
-
             labelcontador.setText(String.valueOf(golpes / 2));
 
         } else if (colision2()) {
