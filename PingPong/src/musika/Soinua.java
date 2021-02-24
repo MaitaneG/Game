428371cc6da95370047a135e7599d57414c95a4f
MaitaneG <75113982+MaitaneG@users.noreply.github.com>
2021-02-24 08:13:02 +0100

    Update Soinua.java

--- PingPong/src/musika/Soinua.java
+++ PingPong/src/musika/Soinua.java
@@ -8,7 +8,9 @@ public class Soinua {
 
     private Clip soinua1;
     private Clip soinua2;
-
+    /**
+     * Soinuaren konstruktorea
+     */
     public Soinua() {
         try {
             this.soinua1 = AudioSystem.getClip();
@@ -23,36 +25,46 @@ public class Soinua {
             System.out.println("" + e);
         }
     }
-
+    
+    /**
+     * Jolasaren musika pizteko
+     */
     public void soinuaHasi() {
         //Soinua hasten da
         soinua1.start();
 
     }
-
+    /**
+     * Jolasaren musika berrizteko (Hau egin dugu hasieran arazoak ematen zigulako)
+     */
     public void soinuaBerriztu() {
 
         soinua1.drain();
 
     }
 
+    /**
+     * Jolasaren musika itzaltzeko
+     */
     public void soinuaItzali() {
 
         // Soinua bukatzen da
         soinua1.close();
 
     }
-
+    /**
+     * Game Over soinua ateratzeko
+     */
     public void gameOverSoinua() {
-        //Bukaerako soinua hasten da
         soinua2.start();
 
     }
-
+    
+    /**
+     * Game over soinua itzaltzeko
+     */
     public void gameOverAmaitu() {
-
         soinua2.close();
-
     }
 
 }
